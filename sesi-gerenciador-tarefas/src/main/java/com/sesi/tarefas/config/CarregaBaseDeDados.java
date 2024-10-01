b7bf055f4915f5d8fc684fc75b3dd094a910d3bf
Barbarha <158178809+Barbarha@users.noreply.github.com>
2024-10-01 12:00:21 -0300

    Add files via upload

---
+++ sesi-gerenciador-tarefas/src/main/java/com/sesi/tarefas/config/CarregaBaseDeDados.java
@@ -0,0 +1,50 @@
+package com.sesi.tarefas.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sesi.tarefas.model.TarefaCategoria;
+import com.sesi.tarefas.model.Usuario;
+import com.sesi.tarefas.repository.TarefaCategoriaRepository;
+import com.sesi.tarefas.repository.UsuarioRepository;
+
+@Configuration
+public class CarregaBaseDeDados {
+
+	@Autowired
+	private TarefaCategoriaRepository tarefaCategoriaRepository;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+		
+	@Bean
+	CommandLineRunner executar() {
+		
+		return args ->{
+			Usuario usuario = new Usuario();
+			usuario.setNome("Barbarha");
+			usuario.setSenha("babi");
+			
+			Usuario usuario1 = new Usuario();
+			usuario.setNome("Fernando");
+			usuario.setSenha("madu♥");
+			
+			
+			TarefaCategoria categoria = new TarefaCategoria();
+			categoria.setNome("Estudos");
+			
+			tarefaCategoriaRepository.save(categoria);
+			
+			
+			TarefaCategoria categoria1 = new TarefaCategoria();
+			categoria1.setNome("Pesquisa");
+			tarefaCategoriaRepository.save(categoria1);
+			
+			
+		};
+		
+	}
+	
+}
